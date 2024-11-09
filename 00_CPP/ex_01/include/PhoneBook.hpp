c0bdef33998debe7aeffdf6979fbea082efc68fc
Adedayo-Sanni <adedayo.dc.sanni@gmail.com>
2024-11-09 20:34:47 -0300

    terminando a implementação da addContact

--- 00_CPP/ex_01/include/PhoneBook.hpp
+++ 00_CPP/ex_01/include/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:55:14 by asanni            #+#    #+#             */
-/*   Updated: 2024/11/07 19:48:19 by asanni           ###   ########.fr       */
+/*   Updated: 2024/11/09 20:29:55 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,19 @@ class PhoneBook
 {
 	private:
 	Contact contact[8];
-	// int num;
+	int index;
+
+	std::string getInfo(std::string prompt){
+	std::string info;
+	
+	std::cout << prompt << std::endl;
+	std::getline(std::cin, info);
+	while (info.empty()) {
+		std::cout << "Input cannot be empty. Type again." << std::endl;
+		std::getline(std::cin, info);
+	}
+	return(info);
+}
 
 	public:
 	void addContact();
--- 00_CPP/ex_01/sources/PhoneBook.cpp
+++ 00_CPP/ex_01/sources/PhoneBook.cpp
@@ -6,43 +6,34 @@
 /*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 18:58:16 by asanni            #+#    #+#             */
-/*   Updated: 2024/11/07 20:48:04 by asanni           ###   ########.fr       */
+/*   Updated: 2024/11/09 20:32:26 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/PhoneBook.hpp"
 
 void PhoneBook::addContact(){
-	std::string firstname;
-	std::cout << "Enter the name" << std::endl;
-	std::getline(std::cin, firstname); 
-	this->contact->setFirstName(firstname);
+	PhoneBook::index = 0;
+	contact->setFirstName(getInfo("Enter the name:"));
+	contact->setLastName(getInfo("Enter the lastname:"));
+	contact->setNickName(getInfo("Enter the nickname"));
+	contact->setPhoneNumber(getInfo("Enter the phonenumber"));
+	contact->setDarkestSecret(getInfo("Enter the darkestsecret"));
 
-	std::string lastname;
-	std::cout << "Enter the lastname" << std::endl;
-	std::getline(std::cin, lastname); 
-	
-	std::string nickname;
-	std::cout << "Enter the nickname" << std::endl;
-	std::getline(std::cin, nickname); 
-
-	std::string phonenumber;
-	std::cout << "Enter the phonenumber" << std::endl;
-	std::getline(std::cin, phonenumber);
-	
-	std::string darkestsecret;
-	std::cout << "Enter the darkestsecret" << std::endl;
-	std::getline(std::cin, darkestsecret); 
-
-	std::cout << contact->getFirstName() << std::endl;
-	
-	//depois criar o contato
+	index = (index + 1) % 8;
 }
+
 void PhoneBook::searchContact(){
 	//Display the saved contacts as a list of 4 columns:
 	//index, first name, last name and nickname.
+	std::cout << contact[index].getFirstName() << std::endl;
+	std::cout << contact[index].getLastName() << std::endl;
+	std::cout << contact[index].getNickName() << std::endl;
+	std::cout << contact[index].getPhoneNumber() << std::endl;
+	std::cout << contact[index].getDarkestSecret() << std::endl;
+	
 
-	//pedir o index do conato que a pessoa quer ver
+	//pedir o index do contato que a pessoa quer ver
 	//se estiver errado ou ainda não existir-> msg de erro
 }
 void PhoneBook::exitPhone(){
