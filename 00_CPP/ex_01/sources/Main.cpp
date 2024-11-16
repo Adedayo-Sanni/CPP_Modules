5fe3c2e9de32867154c7afd45ae51675033014fc
Adedayo-Sanni <adedayo.dc.sanni@gmail.com>
2024-11-16 16:10:36 -0300

    add

--- 00_CPP/ex_01/sources/Main.cpp
+++ 00_CPP/ex_01/sources/Main.cpp
@@ -6,45 +6,12 @@
 /*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 16:22:46 by asanni            #+#    #+#             */
-/*   Updated: 2024/11/16 15:56:33 by asanni           ###   ########.fr       */
+/*   Updated: 2024/11/16 16:04:03 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Contact.hpp"
 #include "../include/PhoneBook.hpp"
-
-#include <iostream>
-#include <string>
-
-// int main(void)
-// {
-// 	std::string input;
-// 	PhoneBook phonebook;
-
-// 	std::cout << "Welcome to your Phonebook!" << std::endl;
-// 	while (true)
-// 	{
-// 		std::cout << "Select your Action: ADD, SEARCH, EXIT" << std::endl;
-// 		std::getline(std::cin, input);
-
-// 		if (!input.compare("ADD")){
-// 			system("clear");
-// 			// std::cout << "\033[2J\033[H";
-// 			phonebook.addContact();
-// 		}
-// 		else if (!input.compare("SEARCH")){
-// 			// std::cout << "\033[2J\033[H";
-// 			phonebook.searchContact();
-// 		}
-// 		else if (!input.compare("EXIT"))
-// 			break;
-// 		else if (!input.empty())
-// 			std::cout << "\nInvalid option. Please try again.\n" << std::endl;
-// 	}
-
-// 	return 0;
-// }
-// arrumar o erro do ctrl D - fzr so sair sem loop infinito
 
 
 int main(void)
