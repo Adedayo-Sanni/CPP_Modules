8caaae833950ead5cd92e0f7f4a79f64a2240242
Adedayo <adedayo.dc.sanni@gmail.com>
2025-10-25 12:57:59 -0300

    add ex01

--- 07_CPP/ex01/iter.hpp
+++ 07_CPP/ex01/iter.hpp
@@ -6,19 +6,23 @@
 /*   By: asanni <asanni@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 12:35:23 by asanni            #+#    #+#             */
-/*   Updated: 2025/10/25 12:35:54 by asanni           ###   ########.fr       */
+/*   Updated: 2025/10/25 12:56:44 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+
 #include <cstddef>
 
-// Template iter: percorre um array e aplica a função f a cada elemento.
-// - T* array: endereço do primeiro elemento (pode ser T = const U).
-// - length: comprimento do array (const unsigned int conforme enunciado).
-// - Func f: qualquer callable (função, ponteiro para função, functor, função-template instanciada).
 template <typename T, typename Func>
 void iter(T* array, const unsigned int length, Func f)
 {
--- 07_CPP/ex01/main.cpp
+++ 07_CPP/ex01/main.cpp
@@ -6,7 +6,81 @@
 /*   By: asanni <asanni@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 12:40:19 by asanni            #+#    #+#             */
-/*   Updated: 2025/10/25 12:40:58 by asanni           ###   ########.fr       */
+/*   Updated: 2025/10/25 12:56:15 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+#include "iter.hpp"
+
+// Imprime um número inteiro (somente leitura)
+void printInt(const int &n)
+{
+    std::cout << n << " ";
+}
+
+// Soma 10 a um número inteiro (modifica o valor)
+void addTen(int &n)
+{
+    n += 10;
+}
+
+//  Imprime uma string
+void printString(const std::string &s)
+{
+    std::cout << s << " ";
+}
+
+// 4️Coloca uma string em maiúsculas (modifica o valor)
+void toUpper(std::string &s)
+{
+    for (std::string::iterator it = s.begin(); it != s.end(); ++it)
+        *it = std::toupper(*it);
+}
+
+// =====================================================
+
+int main(void)
+{
+  std::cout << MAGENTA << "___________int array________________" << RESET <<std::endl;
+    int numbers[] = {1, 2, 3, 4, 5};
+    const unsigned int len = sizeof(numbers) / sizeof(numbers[0]);
+
+    std::cout << "Array original: ";
+    ::iter(numbers, len, printInt);
+    std::cout << std::endl;
+
+    // Aplica addTen em cada elemento → modifica o array
+    ::iter(numbers, len, addTen);
+
+    std::cout << "Após addTen: ";
+    ::iter(numbers, len, printInt);
+    std::cout << std::endl << std::endl;
+
+    std::cout << MAGENTA << "___________const int array________________" << RESET <<std::endl;
+  
+    const int constNumbers[] = {10, 20, 30};
+    const unsigned int len2 = sizeof(constNumbers) / sizeof(constNumbers[0]);
+
+    std::cout << "Array const: ";
+    ::iter(constNumbers, len2, printInt); // só leitura
+    std::cout << std::endl << std::endl;
+
+    std::cout << MAGENTA << "___________string array________________" << RESET <<std::endl;
+    std::string words[] = {"ola", "mundo", "c++"};
+    const unsigned int len3 = sizeof(words) / sizeof(words[0]);
+
+    std::cout << "Strings originais: ";
+    ::iter(words, len3, printString);
+    std::cout << std::endl;
+
+    // Coloca tudo em maiúsculas
+    ::iter(words, len3, toUpper);
+
+    std::cout << "Strings em maiúsculas: ";
+    ::iter(words, len3, printString);
+    std::cout << std::endl;
+
+    return 0;
+}
