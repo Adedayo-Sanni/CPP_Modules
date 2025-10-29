5283aaa8fc4a734f2db5aba486a146047bcf5589
Adedayo <adedayo.dc.sanni@gmail.com>
2025-10-29 20:07:23 -0300

    add correçao na main do ex 02

--- 07_CPP/ex02/main.cpp
+++ 07_CPP/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: asanni <asanni@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 19:36:37 by asanni            #+#    #+#             */
-/*   Updated: 2025/10/29 19:49:49 by asanni           ###   ########.fr       */
+/*   Updated: 2025/10/29 20:03:11 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,32 @@ int main()
         strArr[1] = "World";
         strArr[2] = "C++";
         strArr[3] = "!!";
+        std::cout << std::endl;
 
         for (unsigned int i = 0; i < strArr.size(); ++i)
             std::cout << strArr[i] << " ";
         std::cout << std::endl;
+        
+        
+        std::cout << GREEN << "=== Test non-const Array ===" << RESET << std::endl;
+        Array<int> array(3);
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
 
+        for (unsigned int i = 0; i < array.size(); ++i)
+            std::cout << "array[" << i << "] = " << array[i] << std::endl;
+
+        std::cout << std::endl;
+
+        std::cout << GREEN << "=== Test const Array ===" << RESET << std::endl;
+        const Array<int> arr2 = array; 
+
+        for (unsigned int i = 0; i < arr2.size(); ++i)
+            std::cout << "arr2[" << i << "] = " << arr2[i] << std::endl; // calls const operator[]
+
+        std::cout << std::endl;
+        
         std::cout << GREEN << "=== Test: Try Access Invalid Index ===" << RESET << std::endl;
         std::cout << arr1[7] << std::endl; 
     }
