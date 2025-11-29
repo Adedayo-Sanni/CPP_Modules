601d65582e43f42c01b4b2046e33a7fe14c41183
Adedayo-Sanni <adedayo.dc.sanni@gmail.com>
2025-11-29 13:49:12 -0300

    add corrections

---
+++ life/life.h
@@ -0,0 +1,15 @@
+#ifndef LIFE_H
+#define LIFE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+struct game_init{
+	int height;
+	int width;
+	int it;
+};
+
+#endif
